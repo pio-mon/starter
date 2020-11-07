9ce680dd1343a5d86f56e87b74882b4b48d6f586
Greg Witczak <gogowitczak@gmail.com>
2020-11-07 10:09:32 +0100

    Displaying and sending form

--- src/main/java/com/sda/starter/frontend/ThymeleafController.java
+++ src/main/java/com/sda/starter/frontend/ThymeleafController.java
@@ -1,10 +1,14 @@
 package com.sda.starter.frontend;
 
+import com.sda.starter.frontend.form.GuestbookForm;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Log4j2
 public class ThymeleafController {
 
     private int licznik = 0;
@@ -12,6 +16,13 @@ public class ThymeleafController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("powitanie", "Siemanko! Jesteś " + (++licznik) + " odwiedzającym moją stronę!");
+        model.addAttribute("guestbookForm", new GuestbookForm());
         return "index";
+    }
+
+    @PostMapping("/")
+    public String postIndex(Model model, GuestbookForm guestbookForm) {
+        log.info("Nowy wpis do księgi gości: " + guestbookForm);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/sda/starter/frontend/form/GuestbookForm.java
@@ -0,0 +1,9 @@
+package com.sda.starter.frontend.form;
+
+import lombok.Data;
+
+@Data
+public class GuestbookForm {
+    private String name;
+    private String entry;
+}
