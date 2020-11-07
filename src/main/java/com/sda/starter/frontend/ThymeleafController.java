92ad88dabcedaecda2261ff5097aad1a4c326fbc
Greg Witczak <gogowitczak@gmail.com>
2020-11-07 09:45:35 +0100

    Displaying visitor counter with Thymeleaf

--- src/main/java/com/sda/starter/frontend/ThymeleafController.java
+++ src/main/java/com/sda/starter/frontend/ThymeleafController.java
@@ -1,13 +1,17 @@
 package com.sda.starter.frontend;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafController {
 
+    private int licznik = 0;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("powitanie", "Siemanko! Jesteś " + (++licznik) + " odwiedzającym moją stronę!");
         return "index";
     }
 }
