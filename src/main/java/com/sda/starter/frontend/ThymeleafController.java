b4442733482ff0446244e4578dcf0d07965d4f69
Greg Witczak <gogowitczak@gmail.com>
2020-11-07 09:36:23 +0100

    Displaying homepage with Thymeleaf

---
+++ src/main/java/com/sda/starter/frontend/ThymeleafController.java
@@ -0,0 +1,13 @@
+package com.sda.starter.frontend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
